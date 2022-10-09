767f0713703e9b4987a1c0d9b3a144da0ec4889c
Ardub <ardub92@gmail.com>
2022-10-09 19:32:49 +0300

    test slime

--- assets/scripts/objects/Weapon.cs
+++ assets/scripts/objects/Weapon.cs
@@ -5,25 +5,58 @@ using Galatime;
 public class Weapon : Area2D
 {
     private AnimationPlayer _animation;
+    private float countdown = 0.5f;
+    private bool canAttack = true;
     public override void _Ready()
     {
         _animation = GetNode<AnimationPlayer>("WeaponAnimationPlayer");
-        GD.Print(_animation.ToString());
+
+        // Connect event
         Connect("body_entered", this, "_on_body_entered");
-        
+
+        // Disable collision
+        Monitoring = false;
     }
 
     public void attack() {
-        _animation.Play("swing");
+        if (canAttack)
+        {
+            // Enable collision
+            Monitoring = true;
+
+            // Play animation
+            _animation.Stop();
+            _animation.Play("swing");
+
+            // Delay for disable collision and countdown
+            SceneTree tree = GetTree();
+            tree.CreateTimer(0.05f).Connect("timeout", this, "_resetCollision");
+            tree.CreateTimer(countdown).Connect("timeout", this, "_resetCountdown");
+
+            canAttack = false;
+        }
     } 
 
+    public void _resetCollision() {
+        // Disable collision
+        Monitoring = false;
+    }
+
+    public void _resetCountdown() {
+        canAttack = true;
+    }
+
     public void _on_body_entered(KinematicBody2D body) {
+        // Get scripted node
         Node2D parent = body.GetParent<Node2D>();
+        // !!! NEEDS REWORK !!!
         GalatimeElement element = GalatimeElement.Ignis;
+
+        // Get angle of damage
         float damageRotation = GlobalTransform.origin.AngleToPoint(body.GlobalTransform.origin);
         if (parent.HasMethod("hit"))
         {
-            parent.Call("hit", 10, element, 1000, damageRotation);
+            parent.Call("hit", 10, element, 2000, damageRotation);
         }
     }
 }
--- assets/scripts/objects/enemies/Slime.cs
+++ assets/scripts/objects/enemies/Slime.cs
@@ -4,10 +4,35 @@ using Galatime;
 
 public class Slime : Entity
 {
+    private Node2D _player;
+    private Sprite _sprite;
+    public new float speed = 100;
+
     public override void _Ready()
     {
         element = GalatimeElement.Aqua;
         body = GetNode<KinematicBody2D>("Body");
         damageEffectPoint = GetNode<Position2D>("Body/DamageEffectPoint");
+
+        _player = GetNode<Node2D>("/root/Node2D/player/player_body"); 
+        _sprite = GetNode<Sprite>("Body/Sprite");
+    }
+
+    public override void _moveProcess()
+    {
+        findPath();
+    }
+
+    public void findPath() {
+        Vector2 vectorPath = Vector2.Zero;
+        if (_player != null)
+        {
+            float rotation = body.GlobalTransform.origin.AngleToPoint(_player.GlobalTransform.origin);
+            vectorPath = Vector2.Left.Rotated(rotation) * speed;
+            float rotationDeg = Mathf.Rad2Deg(rotation);
+            float rotationDegPositive = rotationDeg * 1 > 0 ? rotationDeg : -rotationDeg;
+            if (rotationDegPositive >= 90) _sprite.FlipH = true; else _sprite.FlipH = false;
+        } 
+        velocity = vectorPath;
     }
 }
