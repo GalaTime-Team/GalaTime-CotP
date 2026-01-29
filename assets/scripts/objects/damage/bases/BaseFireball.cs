ccdbcb96d476898766e480a631fa10e96671d662
Sérgio Gabriel <106330016+Nihhiu@users.noreply.github.com>
2026-01-29 14:09:31 +0000

    Give values to Enemies and NPCs (except AI rules)

--- assets/scripts/objects/damage/bases/BaseFireball.cs
+++ assets/scripts/objects/damage/bases/BaseFireball.cs
@@ -6,28 +6,27 @@ namespace Galatime.Damage;
 
 public partial class BaseFireball : Node2D, IBaseProjectile
 {
-    public Projectile Projectile { get; set; }
-    public Projectile Proj { get; set; }
-    public AnimationPlayer AnimationPlayer;
+	public Projectile Projectile { get; set; }
+	public Projectile Proj { get; set; }
+	public AnimationPlayer AnimationPlayer;
 
-    public override void _Ready()
-    {
-        Proj = GetNode<Projectile>("Projectile");
-        AnimationPlayer = Proj.GetNode<AnimationPlayer>("AnimationPlayer");
+	public override void _Ready()
+	{
+		Proj = GetNode<Projectile>("Projectile");
+		AnimationPlayer = Proj.GetNode<AnimationPlayer>("AnimationPlayer");
 
-        Proj.Exploded += (Projectile projectile) => AnimationPlayer.Play("outro");
-    }
+		Proj.Exploded += (Projectile projectile) => AnimationPlayer.Play("outro");
+	}
 
-    public void Launch(float direction, float attackStat)
-    {
-        Proj.Rotation = direction;
-        Proj.Visible = true;
-        Proj.Moving = true;
-        Proj.AttackStat = attackStat;
-        Proj.Timeout = 10f;
-        AnimationPlayer.Play("intro");
+	public void Launch(float direction, float attackStat)
+	{
+		Proj.Rotation = direction;
+		Proj.Visible = true;
+		Proj.Moving = true;
+		Proj.AttackStat = attackStat;
+		Proj.Timeout = 10f;
+		AnimationPlayer.Play("intro");
 
-        PlayerVariables.Instance.Player.CameraShakeAmount += 5f;
-    }
+		PlayerVariables.Instance.Player.CameraShakeAmount += 5f;
+	}
 }
-
